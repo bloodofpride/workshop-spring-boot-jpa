5cf0d731b884c0ac3ffbfd0ef9e7e6c9233b9827
bloodofpride <antonio_maxwell@hotmail.com>
2022-03-03 10:35:31 -0300

    User insert

--- src/main/java/com/educandoweb/workshop/controllers/UserController.java
+++ src/main/java/com/educandoweb/workshop/controllers/UserController.java
@@ -4,11 +4,10 @@ import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +24,13 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
